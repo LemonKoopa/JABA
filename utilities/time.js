10bb0b4ce90f03c8a083397d3acaebecfe7fbdc0
LemonKoopa <LemonKoopa+Github@Gmail.com>
2021-05-03 05:50:38 -0500

    Added error checking

--- utilities/time.js
+++ utilities/time.js
@@ -1,9 +1,21 @@
 function convertTime2Seconds (Time)
 	{
   
-  let strTime = Time.split(':'); // Split by : in MM:SSSS:MSMSMS Format
-  let strMinutes = parseInt(strTime[0] * 60); // Convert minutes to seconds
-  let strSeconds = parseInt(strTime[1].substring(0,2)); // Truncate seconds to first 2 digits
-  return (strMinutes + strSeconds);
+  try
+  	{
+    
+      let strTime = Time.split(':'); // Split by : in MM:SSSS:MSMSMS Format
+      let strMinutes = parseInt(strTime[0] * 60); // Convert minutes to seconds
+      let strSeconds = parseInt(strTime[1].substring(0,2)); // Truncate seconds to first 2 digits
+      return (strMinutes + strSeconds);
+      
+  	}
+    
+  catch (error)
   
+  	{
+    
+  		return Time
+      
+  	}
   }
