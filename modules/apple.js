d869b9403a318d59b8e3ade46f7d3bb7a7271977
LemonKoopa <LemonKoopa+Github@Gmail.com>
2021-05-04 10:56:51 -0500

    Changed from Function Object to Class

--- modules/apple.js
+++ modules/apple.js
@@ -1,161 +1,147 @@
-function playerApple (Volume, Shuffle, Loop, isPlaying, Position, Advertisement, Track, isInactive)
-	{
-    
-		this.elementSelectors = {
-			'Volume':'input[aria-label="Volume"]',
-			'Shuffle':'div[aria-label="Shuffle"]',
-			'Loop':'div[aria-label="Repeat one"]',
-			'isPlaying':'button[aria-label="Play"]',
-			'Position':'input[class="web-chrome-playback-lcd__scrub"]',
-			'isInactive':'div[class*="playback-description-not-loaded"]',
-			'Advertisement':'a[data-testid="track-info-advertiser"]',
-			'elementReady':'div[aria-label="Media Controls"]',
-			'elementContainer':'div[aria-label="Media Controls"]',
-			'elementNowPlaying':'div[aria-label="Media Controls"]'
-		}
-    
-		this.elementSelectors.Track = {
-			'Artist':'span[class="web-chrome-playback-lcd__sub-copy-scroll-inner-text-wrapper"] > span:first-child',
-			'Title':'span[class="web-chrome-playback-lcd__sub-copy-scroll-inner-text-wrapper"] > span:nth-child(2)',
-			'Duration':'input[class="web-chrome-playback-lcd__scrub"]'
-		  }
-    
-		this.Volume = Volume;
-		this.Shuffle = Shuffle;
-		this.Loop = Loop;
-		this.isPlaying = isPlaying;
-		this.Position = Position;
-		this.Advertisement = Advertisement;
-		this.Track = function(Artist, Title, Duration)
-			{
-				this.Artist = Artist;
-				this.Title = Title;
-				this.Duration = Duration;
-			}
-      
-		this.History = function(Previous, Current)
-			{
-				this.Previous = [''];
-				this.Current = [''];
-			}
-      
-		this.Controls = new playerControls();
-    
-	}
+class playerApple {
+  constructor(Volume, Shuffle, Loop, isPlaying, isInactive, Advertisement, Artist, Title, Position, Duration, historyPrevious, historyCurrent) {
 
-function playerUpdate (objP)
-	{
-    
-		objP.History.Previous = [objP.Volume, objP.Shuffle, objP.Loop, objP.isPlaying, objP.Position, objP.Advertisement, objP.Track.Artist, objP.Track.Title, objP.Track.Duration];
-			
-		console.log('[JABA-DEBUG] Function (playerUpdate) History.Previous (' + objP.History.Previous + ')');
-		
-    try
-    	{
-        objP.Volume = getElementBySelector(objP.elementSelectors.Volume)?.getAttribute('style').slice(10).split("%")[0];
-        objP.Shuffle = getElementBySelector(objP.elementSelectors.Shuffle).getAttribute('aria-checked') === "true";
-        objP.Loop = document.querySelectorAll(objP.elementSelectors.Loop).length > 0;
-        objP.isPlaying = document.querySelectorAll(objP.Controls.elementSelectors.buttonPause).length > 0;
-        objP.Position = convertTime2Seconds(getElementBySelector(objP.elementSelectors.Position).getAttribute('aria-valuenow'));
-        objP.isInactive = document.querySelectorAll(objP.elementSelectors.isInactive).length > 0;
-        //objP.Advertisement = document.querySelectorAll(objP.elementSelectors.Advertisement).length > 0;
-        objP.Track.Artist = getElementBySelector(objP.elementSelectors.Track.Artist)?.innerText;
-        objP.Track.Title = getElementBySelector(objP.elementSelectors.Track.Title)?.innerText;
-        objP.Track.Duration = convertTime2Seconds(getElementBySelector(objP.elementSelectors.Track.Duration).getAttribute('aria-valuemax'));
-      }
-    catch (error)
-			{
-        objP.Volume = '';
-        objP.Shuffle = '';
-        objP.Loop = '';
-        objP.isPlaying = '';
-        objP.Position = '';
-        objP.Advertisement = '';
-        objP.Track.Artist = '';
-        objP.Track.Title = '';
-        objP.Track.Duration = '';
-      }
-       
+    // Global values
+    this.Volume = Volume;
+    this.Shuffle = Shuffle;
+    this.Loop = Loop;
+    this.isPlaying = isPlaying;
+    this.isInactive = isInactive;
+    this.Advertisement = Advertisement;
 
-		objP.History.Current = [objP.Volume, objP.Shuffle, objP.Loop, objP.isPlaying, objP.Position, objP.Advertisement, objP.Track.Artist, objP.Track.Title, objP.Track.Duration];
+    // Track-specific values
+    this.Artist = Artist;
+    this.Title = Title;
+    this.Duration = Duration;
+    this.Position = Position;
 
-		console.log('[JABA-DEBUG] Function (playerUpdate) History.Current (' + objP.History.Current + ')');
-		
-		if (objP.History.Current.toString() == objP.History.Previous.toString())
-			{
-				return
-			}
+    // Queries
+    this.elementSelector = {
+      'elementReady': 'div[aria-label="Media Controls"]',
+      'elementContainer': 'div[aria-label="Media Controls"]',
+      'elementNowPlaying': 'div[aria-label="Media Controls"]',
+      'Volume': 'input[aria-label="Volume"]',
+      'Shuffle': 'div[aria-label="Shuffle"]',
+      'Loop': 'div[aria-label="Repeat one"]',
+      'isPlaying': 'button[aria-label="Play"]',
+      'isInactive': 'div[class*="playback-description-not-loaded"]',
+      'Advertisement': 'a[data-testid="track-info-advertiser"]',
+      'Artist': 'span[class="web-chrome-playback-lcd__sub-copy-scroll-inner-text-wrapper"] > span:first-child',
+      'Title': 'span[class="web-chrome-playback-lcd__sub-copy-scroll-inner-text-wrapper"] > span:nth-child(2)',
+      'Position': 'input[class="web-chrome-playback-lcd__scrub"]',
+      'Duration': 'input[class="web-chrome-playback-lcd__scrub"]',
+      'buttonVolume': 'button[aria-label="Change volume"]',
+      'buttonShuffle': 'button[aria-label="Shuffle"]',
+      'buttonLoop': 'button[data-testid="control-button-repeat"]',
+      'buttonPrevious': 'button[aria-label="Previous"]',
+      'buttonPause': 'button[aria-label="Pause"]',
+      'buttonPlay': 'button[aria-label="Play"]',
+      'buttonNext': 'button[aria-label="Next"]',
+      'buttonPlaylistPlay': 'button[aria-label="Shuffle"]'
+    }
 
-		console.log('[JABA-INFO] Volume ' + objP.Volume);
-		console.log('[JABA-INFO] Shuffle ' + objP.Shuffle);
-		console.log('[JABA-INFO] Loop ' + objP.Loop);
-		console.log('[JABA-INFO] isPlaying ' + objP.isPlaying);
-		console.log('[JABA-INFO] Position ' + objP.Position);
-		console.log('[JABA-INFO] Advertisement ' + objP.Advertisement);
-		console.log('[JABA-INFO] Artist ' + objP.Track.Artist);
-		console.log('[JABA-INFO] Title ' + objP.Track.Title);
-		console.log('[JABA-INFO] Duration ' + objP.Track.Duration);
+    // Initialize History
+    this.historyPrevious = [];
+    this.historyCurrent = [];
 
-	}
+  }
 
-function playerControls(Previous, Pause, Play, Next, Volume, Shuffle, Loop)
-	{
-		this.elementSelectors = {
-			'buttonPrevious':'button[aria-label="Previous"]',
-			'buttonPause':'button[aria-label="Pause"]',
-			'buttonPlay':'button[aria-label="Play"]',
-			'buttonNext':'button[aria-label="Next"]',
-			'buttonVolume':'button[aria-label="Change volume"]',
-			'buttonShuffle':'button[aria-label="Shuffle"]',
-			'buttonLoop':'button[data-testid="control-button-repeat"]',
-			'buttonPlaylistPlay':'button[aria-label="Shuffle"]'
-		}
-		this.Previous = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Previous (' + this.elementSelectors.buttonPrevious + ')');
-				getElementBySelector(this.elementSelectors.buttonPrevious).click();
-				return null;
-			}
-		this.Pause = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Pause (' + this.elementSelectors.buttonPause + ')');
-				getElementBySelector(this.elementSelectors.buttonPause).click();
-				return null;
-			}
-		this.Play = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Play (' + this.elementSelectors.buttonPlay + ')');
-				getElementBySelector(this.elementSelectors.buttonPlay).click();
-				return null;
-			}
-		this.Next = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Next (' + this.elementSelectors.buttonNext + ')');
-				getElementBySelector(this.elementSelectors.buttonNext).click();
-				return null;
-			}
-		this.Volume = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Volume (' + this.elementSelectors.buttonVolume + ')');
-				getElementBySelector(this.elementSelectors.buttonVolume).click();
-				return null;
-			}
-		this.Shuffle = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Shuffle (' + this.elementSelectors.buttonShuffle + ')');
-				getElementBySelector(this.elementSelectors.buttonShuffle).click();
-				return null;
-			}
-		this.Loop = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Loop (' + this.elementSelectors.buttonLoop + ')');
-				getElementBySelector(this.elementSelectors.buttonLoop).click();
-				return null;
-			}
-		this.playlistPlay = function ()
-			{ 
-				console.log('[JABA] [Controls] Clicking Playlist Play (' + this.elementSelectors.buttonPlaylistPlay + ')');
-				getElementBySelector(this.elementSelectors.buttonPlaylistPlay).click();
-				return null;
-			}
-	}
+  // Callable Functions - Controls
+  controlsVolume() {
+    controlClick(this.elementSelector.buttonVolume);
+  }
+  controlsShuffle() {
+    controlClick(this.elementSelector.buttonShuffle);
+  }
+  controlsLoop() {
+    controlClick(this.elementSelector.buttonLoop);
+  }
+  controlsPrevious() {
+    controlClick(this.elementSelector.buttonPrevious);
+  }
+  controlsPause() {
+    controlClick(this.elementSelector.buttonPause);
+  }
+  controlsPlay() {
+    controlClick(this.elementSelector.buttonPlay);
+  }
+  controlsNext() {
+    controlClick(this.elementSelector.buttonNext);
+  }
+  controlsPlaylistPlay() {
+    controlClick(this.elementSelector.buttonPlaylistPlay);
+  }
+
+  // Callable Functions - History
+  valuesStringify() {
+    console.log('[JABA-DEBUG] Function [valuesStringify] Result [' + +']');
+    let x = [this.Volume, this.Shuffle, this.Loop, this.isPlaying, this.isInactive, this.Advertisement, this.Artist, this.Title, this.Position, this.Duration];
+    return x;
+  }
+
+  valuesUpdate() {
+    console.log('[JABA-DEBUG] Function [valuesUpdate]');
+    this.historyPrevious = this.valuesStringify();
+    this.Volume = this.getVolume();
+    this.Shuffle = this.getShuffle();
+    this.Loop = this.getLoop();
+    this.isPlaying = this.getIsPlaying();
+    this.isInactive = this.getIsInactive();
+    this.Advertisement = this.getAdvertisement();
+    this.Artist = this.getArtist();
+    this.Title = this.getTitle();
+    this.Position = this.getPosition();
+    this.Duration = this.getDuration();
+    this.historyCurrent = this.valuesStringify();
+
+    if (this.historyPrevious.toString() == this.historyCurrent.toString()) {
+      return
+    }
+
+  }
+
+  // Callable Functions - Update Stats - Global Value
+  getVolume() {
+    console.log('[JABA-INFO] Volume ' + this.Volume);
+    return getElementBySelector(this.elementSelector.Volume).getAttribute('style').slice(10).split("%")[0];
+  }
+  getShuffle() {
+    console.log('[JABA-INFO] Shuffle ' + this.Shuffle);
+    return getElementBySelector(this.elementSelector.Shuffle).getAttribute('aria-checked') === "true";
+  }
+  getLoop() {
+    console.log('[JABA-INFO] Loop ' + this.Loop);
+    return document.querySelectorAll(this.elementSelector.Loop).length > 0;
+  }
+  getIsPlaying() {
+    console.log('[JABA-INFO] isPlaying ' + this.isPlaying);
+    return document.querySelectorAll(this.elementSelector.isPlaying).length > 0;
+  }
+  getIsInactive() {
+    console.log('[JABA-INFO] isInactive ' + this.isInactive);
+    return document.querySelectorAll(this.elementSelector.isInactive).length > 0;
+  }
+  getAdvertisement() {
+    console.log('[JABA-INFO] Advertisement ' + this.Advertisement);
+    return document.querySelectorAll(this.elementSelector.Advertisement).length > 0;
+  }
+
+  // Callable Functions - Update Stats - Track-specific Values
+  getArtist() {
+    console.log('[JABA-INFO] Artist ' + this.Artist);
+    return getElementBySelector(this.elementSelector.Artist) ? .innerText;
+  }
+  getTitle() {
+    console.log('[JABA-INFO] Title ' + this.Title);
+    return getElementBySelector(this.elementSelector.Title) ? .innerText;
+  }
+  getPosition() {
+    console.log('[JABA-INFO] Position ' + this.Position);
+    return convertTime2Seconds(getElementBySelector(this.elementSelector.Position).getAttribute('aria-valuenow'));
+  }
+  getDuration() {
+    console.log('[JABA-INFO] Duration ' + this.Duration);
+    return convertTime2Seconds(getElementBySelector(this.elementSelector.Duration).getAttribute('aria-valuemax'));
+  }
+
+}
