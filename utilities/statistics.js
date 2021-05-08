8c289ec04e0884dd24961bec031d623f29edfab7
LemonKoopa <LemonKoopa+Github@Gmail.com>
2021-05-08 07:09:45 -0500

    Added truncating to percents Added calculation of Global Metric - Completion

--- utilities/statistics.js
+++ utilities/statistics.js
@@ -1,6 +1,8 @@
+// let playerStatistics = new Statistics()
+// playerStatistics.addEvent('Artist Name', 'Track Title', Duration, Position when skipped)
+
 class Statistics {
   constructor() {
-
     // Create object array template
     this.data = {
       "Metrics": {
@@ -29,28 +31,17 @@ class Statistics {
         ]
       }
     }
-
     this.length = 0
-
   }
 
   addEvent(Artist, Title, Duration, PlayTime) {
     // Calculate percentage of track played
-    let percentPlayed = PlayTime / Duration * 100
+    let percentPlayed = +(PlayTime / Duration * 100).toFixed(2)
     if (percentPlayed >= 99) {
       percentPlayed = 100
     }
     // Calculate if played to completion or skipped
     let wasSkipped = (percentPlayed !== 100)
-    // Push the above to the statistics
-    if (!!wasSkipped) {
-      this.data.Metrics.Skips += 1
-    } else {
-      this.data.Metrics.Completed += 1
-    }
-    // Add counts to Global Metrics
-    this.data.Metrics.Plays += 1
-    this.data.Metrics.Time += parseInt(PlayTime)
     // Create a Date timestamp
     let thisDate = new Date().toISOString()
     // Push the track information to the statistics
@@ -63,23 +54,39 @@ class Statistics {
       Skipped: wasSkipped,
       Timestamp: thisDate
     })
+    // Add to Global Metrics
+    // Add to Global Metrics - Skips
+    if (!!wasSkipped) {
+      this.data.Metrics.Skips += 1
+    } else {
+      this.data.Metrics.Completed += 1
+    }
+    // Add to Global Metrics - Plays
+    this.data.Metrics.Plays += 1
+    // Add to Global Metrics - Time
+    this.data.Metrics.Time += parseInt(PlayTime)
+    // Add Calculated values
+    this.data.Metrics.Calculated.Completion = +(this.statsCompletion())
   }
 
   // Functions - Array
   getElementAtIndex(index) {
     return this.data[index]
   }
+  
   push(element) {
     this.data[this.length] = element
     this.length++
     return this.length
   }
+  
   pop() {
     const item = this.data[this.length - 1]
     delete this.data[this.length - 1]
     this.length--
     return this.data
   }
+  
   deleteAt(index) {
     for (let i = index; i < this.length - 1; i++) {
       this.data[i] = this.data[i + 1]
@@ -88,6 +95,7 @@ class Statistics {
     this.length--
     return this.data
   }
+  
   insertAt(item, index) {
     for (let i = this.length; i >= index; i--) {
       this.data[i] = this.data[i - 1]
@@ -99,6 +107,17 @@ class Statistics {
 
   Stringify() {
     return JSON.stringify(this.data)
+  }
+
+  statsCompletion() {
+    let completionTotal = 0
+    let completionPercent = 0
+    this.data.Metrics.Tracks.forEach(entry => {
+      let entryCompletion = entry.Completion
+      completionTotal += entryCompletion
+    })
+    completionPercent = (completionTotal / this.data.Metrics.Tracks.length).toFixed(2)
+    return completionPercent
   }
 
 }
