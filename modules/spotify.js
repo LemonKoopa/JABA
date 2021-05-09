932d8d42f87f8eaa002287e32c86aecca406a438
LemonKoopa <LemonKoopa+Github@Gmail.com>
2021-05-08 23:58:50 -0500

    Off-loaded non-specific functions to _generic.js

--- modules/spotify.js
+++ modules/spotify.js
@@ -1,23 +1,6 @@
-class playerSpotify {
+class playerSpotify extends _generic {
   constructor() {
-
-    // Global values
-    this.Volume = null
-    this.Shuffle = null
-    this.Loop = null
-    this.isPlaying = null
-    this.isInactive = null
-    this.Advertisement = null
-
-    // Track-specific values
-    this.Artist = null
-    this.Title = null
-    this.Duration = null
-    this.Position = null
-	
-	// Initialize History
-    this.history = {}
-	
+		super()
     // Queries
     this.elementSelector = {
       'elementReady': 'div[data-testid="playlist-tracklist"]',
@@ -71,56 +54,6 @@ class playerSpotify {
     controlClick(this.elementSelector.buttonPlaylistPlay)
   }
 
-  // Callable Functions - History
-  updateHistory() {
-    this.history = {
-      Volume: this.Volume,
-      Shuffle: this.Shuffle,
-      Loop: this.Loop,
-      isPlaying: this.isPlaying,
-      isInactive: this.isInactive,
-      Advertisement: this.Advertisement,
-      Artist: this.Artist,
-      Title: this.Title,
-      Position: this.Position,
-      Duration: this.Duration
-    }
-  }
-  
-  // Returns an object array of changed values { key: 'newValue', 'oldValue' }
-  hasChanged() {
-    let changedValues = []
-    Object.entries(this).forEach(entry => {
-      let entryKey = entry[0]
-      let entryValue = entry[1]
-      if (entryKey == 'history') { return }
-      if (entryKey == 'elementSelector') { return }
-      if (entryValue == this.history[entryKey]) { return }
-      changedValues[entryKey] = [entryValue, this.history[entryKey]]
-    })
-    return changedValues
-  }
-
-  // Callable Functions - History - Update values from DOM
-  valuesUpdate() {
-    this.updateHistory()
-    
-    this.Volume = this.getVolume()
-    this.Shuffle = this.getShuffle()
-    this.Loop = this.getLoop()
-    this.isPlaying = this.getIsPlaying()
-    this.isInactive = this.getIsInactive()
-    this.Advertisement = this.getAdvertisement()
-    this.Artist = this.getArtist()
-    this.Title = this.getTitle()
-    this.Position = this.getPosition()
-    this.Duration = this.getDuration()
-	  // Display information to console if anything has updated
-    if (!!Object.keys(this.hasChanged()).length) {
-    //console.info('[JABA] ', this.hasChanged())
-	  }
-  }
-  
   // Callable Functions - Update Stats - Global Value
   getVolume() {
     return getElementBySelector(this.elementSelector.Volume).getAttribute('style').slice(6).split("%")[0].split(".")[0]
